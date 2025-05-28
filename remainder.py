a366ba0b6a53b3f018075f4ba1ccb2fb392b426e
nganesh <nganesh@iu.edu>
2025-05-27 19:28:22 -0700

    remainder

---
+++ remainder.py
@@ -0,0 +1,8 @@
+numerator = int(input("Enter your numerator:"))
+denominator = int(input("Enter your denominator:"))
+
+if numerator%denominator==0:
+    print("Is divisible.")
+else:
+    print("Is not divisible.")
+
