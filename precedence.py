b4f628866c33a3a0f8e0f6dfb9512429d85f8bd9
nganesh <nganesh@iu.edu>
2025-05-27 19:22:52 -0700

    precedence

---
+++ precedence.py
@@ -0,0 +1,17 @@
+x = 7
+y = 8
+n = 18
+s = 14
+
+z = (n+s)*x + y; 
+print("The value of the expression (n+s)**x + y is", z)
+
+
+time = "5:00"
+place = "hawaii"
+age = 19
+
+if time == "5:00" or place == "cabo" or age <= 18:
+    print("hi")
+else:
+    print("bye")
